bb05e31205059c48623ad811a1e57fe2f5321472
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-03-19 14:44:06 -0600

    formatted

--- awsTest.js
+++ awsTest.js
@@ -3,10 +3,9 @@ const AWS = require('aws-sdk');
 let s3 = new AWS.S3();
 
 s3.listBuckets((err, data) => {
-    if (err){
-        console.log("Error: ", err);
-    }
-    else {
-        console.log("Bucket List: ", data.Buckets)
-    }
-})
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    console.log('Bucket List: ', data.Buckets);
+  }
+});
